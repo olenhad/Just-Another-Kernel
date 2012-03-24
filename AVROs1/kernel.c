7033c4418af0a4dc29e9c7cae27ba8afc684f556
Omer <momeriqbal2@gmail.com>
2012-03-24 19:21:31 +0800

    Added OSRun() code, and modified ttcb to contain a runCount instead of runFlag. Starting on OSSwap()

--- AVROs1/kernel.c
+++ AVROs1/kernel.c
@@ -11,12 +11,14 @@
 #include <avr/interrupt.h>
 #include "kernel.h"
 
+int currentTask = -1;
 unsigned long pxCurrentTCB;
 unsigned numTasks=0;
 // Task Control Block
 typedef struct ttcb
 {
-	int runflag; // Shows whether task has been run before or not. 0 = first time this task is being run, 
+	int runCount;
+	//int runflag; // Shows whether task has been run before or not. 0 = first time this task is being run, 
 				// 1 = task has been run before and was pre-empted/surrendered control.
 				 
 	int prio; // Task Priority.
@@ -27,100 +29,6 @@ typedef struct ttcb
 
 // Task Control Block (TCB), which contains information on all the tasks.
 TTaskBlock taskTable[OS_NUM_TASKS];
-
-// Macros to save and restore context. DO NOT CHANGE THESE!
-#define portSAVE_CONTEXT()\
-asm volatile (\
-	"push r0	\n\t"\
-	"in r0, __SREG__	\n\t"\
-	"cli	\n\t"\
-	"push r0	\n\t"\
-	"push r1	\n\t"\
-	"clr r1	\n\t"\
-	"push r2	\n\t"\
-	"push r3	\n\t"\
-	"push r4	\n\t"\
-	"push r5	\n\t"\
-	"push r6	\n\t"\
-	"push r7	\n\t"\
-	"push r8	\n\t"\
-	"push r9	\n\t"\
-	"push r10	\n\t"\
-	"push r11	\n\t"\
-	"push r12	\n\t"\
-	"push r13	\n\t"\
-	"push r14	\n\t"\
-	"push r15	\n\t"\
-	"push r16	\n\t"\
-	"push r17	\n\t"\
-	"push r18	\n\t"\
-	"push r19	\n\t"\
-	"push r20	\n\t"\
-	"push r21	\n\t"\
-	"push r22	\n\t"\
-	"push r23	\n\t"\
-	"push r24	\n\t"\
-	"push r25	\n\t"\
-	"push r26	\n\t"\
-	"push r27	\n\t"\
-	"push r28	\n\t"\
-	"push r29	\n\t"\
-	"push r30	\n\t"\
-	"push r31	\n\t"\
-	"in r26, __SP_L__	\n\t"\
-	"in r27, __SP_H__	\n\t"\
-	"sts pxCurrentTCB+1, r27	\n\t"\
-	"sts pxCurrentTCB, r26	\n\t"\
-	"sei	\n\t" : : :\
-);
-
-#define portRESTORE_CONTEXT()\
-asm volatile (\
-	"out __SP_L__, %A0	\n\t"\
-	"out __SP_H__, %B0	\n\t"\
-	"pop r31	\n\t"\
-	"pop r30	\n\t"\
-	"pop r29	\n\t"\
-	"pop r28	\n\t"\
-	"pop r27	\n\t"\
-	"pop r26	\n\t"\
-	"pop r25	\n\t"\
-	"pop r24	\n\t"\
-	"pop r23	\n\t"\
-	"pop r22	\n\t"\
-	"pop r21	\n\t"\
-	"pop r20	\n\t"\
-	"pop r19	\n\t"\
-	"pop r18	\n\t"\
-	"pop r17	\n\t"\
-	"pop r16	\n\t"\
-	"pop r15	\n\t"\
-	"pop r14	\n\t"\
-	"pop r13	\n\t"\
-	"pop r12	\n\t"\
-	"pop r11	\n\t"\
-	"pop r10	\n\t"\
-	"pop r9	\n\t"\
-	"pop r8	\n\t"\
-	"pop r7	\n\t"\
-	"pop r6	\n\t"\
-	"pop r5	\n\t"\
-	"pop r4	\n\t"\
-	"pop r3	\n\t"\
-	"pop r2	\n\t"\
-	"pop r1	\n\t"\
-	"pop r0	\n\t"\
-	"out __SREG__, r0\n\t"\
-	"pop r0	\n\t": : "r" (pxCurrentTCB):\
-	);
-
-#define MOVSP()\
-asm volatile ("cli \n\t"\
-				"lds r26, pxCurrentTCB \n\t"\
-				"lds r27, pxCurrentTCB+1 \n\t"\
-				"out __SP_L__, r26 \n\t"\
-				"out __SP_H__, r27 \n\t"\
-				"sei");
 
 void OSMakeAtomic()
 {
@@ -151,7 +59,7 @@ void OSSwapTask()
 	// Important: If you are starting up a task for the first time, you must set pxCurrentTCB
 	// to be equal to the address of the stack allocated to the task in OSAddTask, then
 	// call MOVSP to correctly set up SP_L and SP_H.
-		
+	
 
 // Do not modify the line below!	
 	asm("ret");
@@ -185,31 +93,21 @@ int OSAddTask(void (*taskptr)(void *), int prio, int *stack, void* arg)
 	// Adds a new entry to the TCB. Returns a -1 if # of tasks > OS_NUM_TASKS. Note that OS_NUM_TASKS is the MAXIMUM
 	// number of tasks that can be created, not the actual number.
 	
-	TTaskBlock *tempTask;
-	tempTask->fptr()= taskptr;
-	tempTask->arg = arg;
-	tempTask->prio = prio;
-	tempTask->stack_ptr = stack;
-	tempTask->runflag=0;
+	TTaskBlock tempTask;
+	tempTask.fptr()= taskptr;
+	tempTask.arg = arg;
+	tempTask.prio = prio;
+	tempTask.stack_ptr = stack;
+	tempTask.runCount=0;
 	taskTable[numTasks] = tempTask;
 	
 	numTasks++;
 
-	return 0;	
+	return 0;
 }
 
 void OSRun()
 {
-	// Starts by executing the first task
-	int i= 0;
-	taskTable[0]->runflag=0;
-	taskTable[0]->fptr(taskTable[0]->arg);
-	while(1){
-		
-		OSSwapTask()
-		i=findNextTask()
-		taskTable[i]->fptr(taskTable[i]->arg);
-		
-	}
-	
+	taskTable[0].runCount++;
+	taskTable[0].fptr((void*)taskTable[0].arg);
 }
